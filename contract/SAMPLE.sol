4dc2602023e7de2415a12b5856c7289b6c3a9cea
Gideon Aduku <gideonaduku@gmail.com>
2023-01-10 20:34:52 +0000

    Update SAMPLE.sol
    This smart contract provides a daily return of 0.25% on deposits and has a referral bonus of 3% on the initial deposit sent to referrers. There is also a 3% development/marketing fee on deposits and withdrawals. 25% of deposits are sent to a trading (salsa) wallet, and users can compound their rewards twice, with a mandatory waiting period of 24 hours between compounds. There is a 30 hour withdrawal window after the last compound, after which rewards will stop accumulating until a compound or withdrawal is made.
    Additionally, users can deposit funds into the salsa pool, which are then invested into other investments and the user's share of rewards from the investment is credited to the user's rewards balance in the main balance.
    A 1% fee for the management and for gas fees, are sent to the owner's wallet upon deposit and each time the owner deposits investment rewards.
    Finally, the contract has a lottery feature, where a user can buy a lottery ticket with a click and the price is set to 10 BUSD. Once 10 tickets are purchased, 50 BUSD is sent to a random winner and 50 BUSD is kept in the contract. The last lottery winner is stored and replaced each time the lottery finishes.

--- contract/SAMPLE.sol
+++ contract/SAMPLE.sol
@@ -1,41 +1,135 @@
-solidity ^0.8.11;
+pragma solidity 0.8.11;
 
-contract DepositAndEarn {
-  address public salsaTradingWallet;
-  address public marketingWallet;
-  mapping (address => uint256) public deposits;
-  mapping (address => uint256) public earned;
-  mapping (address => uint256) public lastCompoundTime;
-
-  constructor(address _salsaTradingWallet, address _marketingWallet) public {
-    salsaTradingWallet = _salsaTradingWallet;
-    marketingWallet = _marketingWallet;
-  }
-
-  // User makes a deposit
-  function deposit() public payable {
-    // Calculate referral bonus
-    address referrer = msg.sender.call(abi.encodePacked("referrer"));
-    if (referrer != address(0)) {
-      uint256 referralBonus = (deposits[msg.sender] * 3) / 100;
-      referrer.transfer(referralBonus);
+contract DailyReturns {
+    // struct to hold user account information
+    struct User {
+        address referrer;
+        uint256 initialDeposit;
+        uint256 lastCompoundTime;
+        uint256 rewards;
+        bool isEligibleForRewards;
     }
 
-    // Calculate development/marketing fee
-    uint256 devMarketingFee = (msg.value * 3) / 100;
-    marketingWallet.transfer(devMarketingFee);
+    // mapping to hold user account information
+    mapping(address => User) public users;
 
-    // Calculate trading fee
-    uint256 tradingFee = (msg.value * 25) / 100;
-    salsaTradingWallet.transfer(tradingFee);
+    // salsa pool information
+    address payable salsaPool;
+    mapping(address => uint256) public salsaShares;
 
-    // Update user deposit and earned amounts
-    deposits[msg.sender] += msg.value;
-    earned[msg.sender] += (deposits[msg.sender] * 0.25) / 100;
-  }
+    // lottery information
+    address lotteryWinner;
+    uint256 lotteryPool;
+    uint256 lotteryTicketPrice;
 
-  /**
-  TODO
-  User compounds
-   */
+    constructor() public {
+        salsaPool = address(0x0);
+        lotteryTicketPrice = 10 ether;
+    }
+
+    // function to deposit funds and set referrer if applicable
+    function deposit(address payable _referrer) public payable {
+        // get user account information
+        User storage user = users[msg.sender];
+
+        // if user has no initial deposit, set referrer and initial deposit
+        if (user.initialDeposit == 0) {
+            user.referrer = _referrer;
+            user.initialDeposit = msg.value;
+        }
+
+        // add deposit to user rewards
+        user.rewards += msg.value * (1 - 0.03 - 0.03);
+
+        // add referral bonus to referrer
+        if (_referrer != address(0x0)) {
+            User storage referrer = users[_referrer];
+            referrer.rewards += msg.value * 0.03;
+        }
+
+        // add deposit to salsa pool
+        salsaShares[msg.sender] += msg.value * 0.25;
+    }
+
+    // function to compound rewards
+    function compound() public {
+        // get user account information
+        User storage user = users[msg.sender];
+
+        // check if user is eligible for compounding
+        if (user.isEligibleForRewards && user.lastCompoundTime + 24 hours <= now) {
+            // add rewards to deposit
+            user.initialDeposit += user.rewards;
+
+            // reset rewards and update last compound time
+            user.rewards = 0;
+            user.lastCompoundTime = now;
+        }
+    }
+
+    // function to withdraw rewards
+    function withdraw() public {
+        // get user account information
+        User storage user = users[msg.sender];
+
+        // check if user is eligible for withdrawal
+        if (user.isEligibleForRewards && user.lastCompoundTime + 30 hours <= now) {
+            // transfer rewards to user
+            require(user.rewards > 0, "No rewards to withdraw.");
+            msg.sender.transfer(user.rewards);
+
+            // reset rewards and set ineligibility for rewards
+            user.rewards = 0;
+            user.isEligibleForRewards = false;
+        }
+    }
+
+    function AddSalsa() public payable {
+        require(msg.value > 0, "Invalid deposit amount.");
+        salsaPool.transfer(msg.value);
+    }
+
+      
+    function distributionSalsa() public {
+        require(salsaPool.balance > 0, "No rewards to distribute.");
+
+        // calculate total shares in salsa pool
+        uint256 totalShares = 0;
+        for (address user in salsaShares) {
+            totalShares += salsaShares[user];
+        }
+
+        // distribute rewards to users
+        for (address user in salsaShares) {
+            uint256 share = salsaShares[user] / totalShares;
+            uint256 rewards = salsaPool.balance * share;
+            users[user].rewards += rewards;
+            salsaShares[user] = 0;
+        }
+
+        // transfer 1% of rewards to owner's wallet
+        address owner = msg.sender;
+        owner.transfer(salsaPool.balance * 0.01);
+        salsaPool.transfer(salsaPool.balance * 0.99);
+    }
+
+    // function to buy lottery ticket
+    function buyLotteryTicket() public payable {
+        require(msg.value == lotteryTicketPrice, "Invalid ticket price.");
+
+        // add ticket price to lottery pool
+        lotteryPool += msg.value;
+
+        // if lottery pool has enough tickets for a draw
+        if (lotteryPool >= 10 * lotteryTicketPrice) {
+            // select winner
+            lotteryWinner = address(uint160(keccak256(abi.encodePacked(block.timestamp, block.difficulty))) % lotteryPool);
+
+            // transfer winnings to winner
+            lotteryWinner.transfer(lotteryPool / 2);
+
+            // reset lottery pool
+            lotteryPool = 0;
+        }
+    }
 }
